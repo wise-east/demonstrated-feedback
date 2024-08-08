5571f537ac93505ea391341c1169a06aa1c5018e
Justin Cho <hd.justincho@gmail.com>
2024-08-08 18:15:54 +0000

    fix author id list for cmcc

--- generate_all.sh
+++ generate_all.sh
@@ -2,18 +2,25 @@
 # . generate.sh speechpref 0 ditto 1 -> generate for speechpref baseline for author 0 using model with ditto method on device 1
 
 
-. generate.sh speechpref 0 ditto 0
-. generate.sh speechpref 0 sft 1 
-. generate.sh speechpref 0 zero 2
-. generate.sh speechpref 0 few 3
+# methods=(ditto sft zero few)
+methods=(ditto)
 
-methods=(ditto sft zero few)
+benchmark="cmcc" # cmcc, ccat50, speechpref 
 
-for method in ${methods[@]}; do
-    for author in {0..9}; do
-        . generate.sh speechpref $author $method 0
-    done    
+authors=(0 1 2 3 4)
+
+N=8 # set to number of gpus available 
+
+for method in ${methods[@]}; do 
+    for author in ${authors[@]}; do 
+        ((j=j%N)); ((j++==0)) && wait
+        
+        gpu_id=$(($j % $N))
+        . generate.sh $benchmark $author $method $gpu_id &
+
+    done; 
 done
 
-methods=(ditto sft zero few); for method in ${methods[@]}; do for author in {0..9}; do . generate.sh ccat50 $author $method 0; done; done
-methods=(ditto sft zero few); for method in ${methods[@]}; do for author in {0..9}; do . generate.sh cmcc $author $method 1; done; done
+wait
+
+echo "all generations complete"
